04fc94d33bc2ac32f51d58e0fb2cd1bf06dbb99c
micro <648580211@qq.com>
2016-12-22 00:17:29 +0800

    Update Zhang_fu_model.php

--- application/models/Zhang_fu_model.php
+++ application/models/Zhang_fu_model.php
@@ -5,8 +5,8 @@ class Zhang_fu_model extends CI_Model {
         $this->load->database();
         $this->liutong_max = 90;
         $this->zuixin_min = 8;
-        $this->zuixin_max = 30;
-        $this->shijing_max = 5;
+        $this->zuixin_max = 60;
+        $this->shijing_max = 15;
         $this->zhangfu_min = -0.005;
         $this->zhangfu_max = 0.025;
         $this->zhangfu_min_4 = -0.075;
