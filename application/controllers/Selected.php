0dd8a4f5d8f842ea8f5dd4311430d5ec5b22f21b
micro <648580211@qq.com>
2017-02-24 19:24:16 +0800

    Update Selected.php

--- application/controllers/Selected.php
+++ application/controllers/Selected.php
@@ -4,7 +4,12 @@ class Selected extends CI_Controller {
 	public function index()
 	{
 		$this->load->model('stock_list_model');
-        $data['stock_list'] = $this->stock_list_model->selected();
-        $this->load->view('diefu/diefu', $data);
+        	$data['stock_list'] = $this->stock_list_model->get_stock_selected();
+		
+		for ($index = 0, $max = count($data['stock_list']); $index < $max; $index++) {
+		    $data['stock_list'][$index]['selected'] = true;
+		}
+		
+        	$this->load->view('chengjiaoliang/chengjiaoliang', $data);
 	}
 }
