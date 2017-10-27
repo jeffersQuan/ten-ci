24fdde51f19a3d1a4b69064d5c31a4e0a7043b70
quan.jf <quan.jf@yuwan8.com>
2017-10-27 19:20:00 +0800

    update

--- application/models/Cheng_jiao_liang_model.php
+++ application/models/Cheng_jiao_liang_model.php
@@ -35,78 +35,12 @@ class Cheng_jiao_liang_model extends CI_Model
 
     public function back_up()
     {
-        $query = $this->db->query('select * from ten_chengjiaoliang');
-        foreach ($query->result() as $row) {
-            $code = $row->code;
-            $d0 = $row->d0;
-            $d1 = $row->d1;
-            $d2 = $row->d2;
-            $d3 = $row->d3;
-            $d4 = $row->d4;
-            $d5 = $row->d5;
-            $d6 = $row->d6;
-            $d7 = $row->d7;
-            $d8 = $row->d8;
-            $d9 = $row->d9;
-            $d10 = $row->d10;
-            $d11 = $row->d11;
-            $d12 = $row->d12;
-            $d13 = $row->d13;
-            $d14 = $row->d14;
-            $d15 = $row->d15;
-            $d16 = $row->d16;
-            $d17 = $row->d17;
-            $d18 = $row->d18;
-            $d19 = $row->d19;
-            $d20 = $row->d20;
-            $d21 = $row->d21;
-            $d22 = $row->d22;
-            $d23 = $row->d23;
-            $d24 = $row->d24;
-            $d25 = $row->d25;
-            $d26 = $row->d26;
-            $d27 = $row->d27;
-            $d28 = $row->d28;
-            $d29 = $row->d29;
-            $d30 = $row->d30;
-            $d31 = $row->d31;
-            $d32 = $row->d32;
-            $d33 = $row->d33;
-            $d34 = $row->d34;
-            $d35 = $row->d35;
-            $d36 = $row->d36;
-            $d37 = $row->d37;
-            $d38 = $row->d38;
-            $d39 = $row->d39;
-            $d40 = $row->d40;
-            $d41 = $row->d41;
-            $d42 = $row->d42;
-            $d43 = $row->d43;
-            $d44 = $row->d44;
-            $d45 = $row->d45;
-            $d46 = $row->d46;
-            $d47 = $row->d47;
-            $d48 = $row->d48;
-            $d49 = $row->d49;
-            $d50 = $row->d50;
-            $d51 = $row->d51;
-            $d52 = $row->d52;
-            $d53 = $row->d53;
-            $d54 = $row->d54;
-            $d55 = $row->d55;
-            $d56 = $row->d56;
-            $d57 = $row->d57;
-            $d58 = $row->d58;
-            $d59 = $row->d59;
-
-            $this->db->query("update ten_chengjiaoliang set d0=0,d1=$d0,d2=$d1,d3=$d2,d4=$d3,d5=$d4,d6=$d5,d7=$d6,
-               d8=$d7,d9=$d8,d10=$d9,d11=$d10,d12=$d11,d13=$d12,d14=$d13,d15=$d14,d16=$d15,d17=$d16,
-               d18=$d17,d19=$d18,d20=$d19,d21=$d20,d22=$d21,d23=$d22,d24=$d23,d25=$d24,d26=$d25,d27=$d26,
-               d28=$d27,d29=$d28,d30=$d29,d31=$d30,d32=$d31,d33=$d32,d34=$d33,d35=$d34,d36=$d35,d37=$d36,
-               d38=$d37,d39=$d38,d40=$d39,d41=$d40,d42=$d41,d43=$d42,d44=$d43,d45=$d44,d46=$d45,d47=$d46,
-               d48=$d47,d49=$d48,d50=$d49,d51=$d50,d52=$d51,d53=$d52,d54=$d53,d55=$d54,d56=$d55,d57=$d56,
-               d58=$d57,d59=$d58,d60=$d59 where code='$code'");
-        }
+        $this->db->query("update ten_chengjiaoliang set d60=d59,d59=d58,d58=d57,d57=d56,d56=d55,d55=d54,d54=d53,d53=d52,d52=d51,d51=d50,
+                          d50=d49,d49=d48,d48=d47,d47=d46,d46=d45,d45=d44,d44=d43,d43=d42,d42=d41,d41=d40,
+                          d40=d39,d39=d38,d38=d37,d37=d36,d36=d35,d35=d34,d34=d33,d33=d32,d32=d31,d31=d30,
+                          d30=d29,d29=d28,d28=d27,d27=d26,d26=d25,d25=d24,d24=d23,d23=d22,d22=d21,d21=d20,
+                          d20=d19,d19=d18,d18=d17,d17=d16,d16=d15,d15=d14,d14=d13,d13=d12,d12=d11,d11=d10,
+                          d10=d9,d9=d8,d8=d7,d7=d6,d6=d5,d5=d4,d4=d3,d3=d2,d2=d1,d1=d0,d0=0");
     }
 }
 
